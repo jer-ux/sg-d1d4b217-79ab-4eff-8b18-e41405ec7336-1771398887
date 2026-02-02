bc515f817110bfb98af53e1ab75beed4302f7fef
softgenai <admin@softgen.ai>
2026-02-02 17:48:24 +0000

    feat(app): Add universal Nav component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import "@/styles/main.css";
 import { ThemeProvider } from "@/contexts/ThemeProvider";
 import { ToasterMenu } from "@/components/ToasterMenu";
+import Nav from "@/components/Nav";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
       <div className="dark">
+        <Nav />
         <Component {...pageProps} />
         <ToasterMenu />
       </div>

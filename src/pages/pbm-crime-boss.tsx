dc48f131337c162b4bea1e23e21833b2a381997f
softgenai <admin@softgen.ai>
2026-05-21 01:05:14 +0000

    chore(ui): remove pbm crime boss calculator and helper

--- src/pages/pbm-crime-boss.tsx
+++ src/pages/pbm-crime-boss.tsx
@@ -91,15 +91,6 @@ export default function PBMCrimeBossPage() {
     setLightboxImage(allImages[newIndex]);
   };
 
-  const formatCurrency = (amount: number) => {
-    return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 0,
-      maximumFractionDigits: 0
-    }).format(amount);
-  };
-
   return (
     <>
       <Head>
